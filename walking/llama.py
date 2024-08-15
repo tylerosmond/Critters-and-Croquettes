8140cccfe8dfb99a18e968ef2be7aa8855720d16
Tyler Osmond <140542331+tylerosmond@users.noreply.github.com>
2024-08-15 15:33:30 -0500

    added animal shifts to all 'walking' animals

--- walking/llama.py
+++ walking/llama.py
@@ -3,10 +3,11 @@ from datetime import date
 
 class Llama:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
+        self.shift = shift
         self.walking = True
         self.date_added = date.today()
