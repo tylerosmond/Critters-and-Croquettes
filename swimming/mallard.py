5f411087342b5726c2080d861b956aa608fd2ad4
Tyler Osmond <140542331+tylerosmond@users.noreply.github.com>
2024-08-16 09:42:26 -0500

    Added a feed method to all animal classes and schedule method to all walking animals

--- swimming/mallard.py
+++ swimming/mallard.py
@@ -3,8 +3,12 @@ from datetime import date
 
 class Mallard:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
+        self.food = food
         self.swimming = True
         self.date_added = date.today()
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
--- walking/kangaroo.py
+++ walking/kangaroo.py
@@ -3,9 +3,18 @@ from datetime import date
 
 class Kangaroo:
 
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.shift = shift
+        self.food = food
         self.walking = True
         self.date_added = date.today()
+
+    def schedule(self):
+        print(
+            f"{self.name}, the {self.species}, is available to pet during the {self.shift} shift."
+        )
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
